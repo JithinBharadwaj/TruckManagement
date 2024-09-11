3004b7f8b849cab719268c2d16299c883f760998
Jithin Bharadwaj <jithinbharadwaj66@gmail.com>
2024-09-11 15:24:56 +0530

    Second Commit

--- src/main/java/org/example/ConnectionDetails.java
+++ src/main/java/org/example/ConnectionDetails.java
@@ -9,7 +9,7 @@ public class ConnectionDetails {
     public static final String LOADDRIVER = "com.mysql.cj.jdbc.Driver";
     public static final String URL = "jdbc:mysql://localhost:3306/truck_Management";
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "Buncy@143";
+    public static final String PASSWORD = "*****";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL,USERNAME,PASSWORD);
